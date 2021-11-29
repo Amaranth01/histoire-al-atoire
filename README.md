6b590ab8f24bbb1e68cf18decf817948ee53f5b1
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-29 18:24:09 +0100

    Project history ok

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# histoire-al-atoire
+# histoire-aléatoire
+
+créer les tableaux. OK
+Récupérer le nom des éléments. OK
+créer le bouton sur le HTML puis l'apporter dans le JS, pareil pour le champ de saisi. OK
+
+Définir les champs pris au hasard.
+initialiser le click sur le bouton.
+
+Faire en sorte que le nom de l'utilisateur soit pris dans l'histoire.
+
+En dernier tout intégrer dans le DOM.
+
+
---
+++ script.js
@@ -0,0 +1,15 @@
+let array1 = [' et Erwin', ' et Anya', ' et Jenifael', ' et Ellana', ' et l\'Impératrice'];
+let array2 = ['avec leur Epée', 'avec leur Lame empoisonnée', 'avec leur passe-partout', 'avec leurs Griffes', 'avec leur cristal'];
+let array3 = ['dans la neige', 'avec 15 degrés', 'sous le soleil', 'par -20 degrés', "alors qu'il fait froid"];
+let array4 = ['au Royaume d\'Emeraude', 'à la Cité Altheraan', 'à la Cité celeste', 'au Temple des Marchombres', 'au Château'];
+let array5 = ["glandent", 'se baignent', 'son allongés', 'programme', 'chantent'];
+
+let history = document.getElementById('history');
+let button = document.getElementById('button');
+let input = document.getElementById('input');
+
+button.addEventListener("click", function (){
+    let aleatory = Math.round(Math.random()*4);
+    history.innerHTML += input.value + array1[aleatory]+ ' ' + array5[aleatory] + ' ' + array2[aleatory] + ' ' + array3 [aleatory] + ' ' + array4[aleatory] + "<br>";
+});
+
